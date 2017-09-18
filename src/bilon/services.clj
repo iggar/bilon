d673058c19cc76bcaf7f87fca59843dcebcca318
Igor Garcia <igarciauk@gmail.com>
2017-09-19 00:05:07 +0100

    Generate simplified list from feed with name, bikes available and coordinates

--- src/bilon/services.clj
+++ src/bilon/services.clj
@@ -6,3 +6,24 @@
   (let [feed (json/read-str
                 (:body (client/get url {:accept :json})) :key-fn keyword)]
       (take n feed)))
+
+(defn bikes-available [bikepoint]
+  (let [additional-props (:additionalProperties bikepoint)]
+    (first (remove nil? (map #(if (= "NbBikes" (:key %)) (:value %)) additional-props)))))
+
+(defn point-name [bikepoint]
+  (:commonName bikepoint))
+
+(defn point-lat-lon [bikepoint]
+  (vector (:lat bikepoint) (:lon bikepoint)))
+
+(defn point->simplepoint [bikepoint]
+  {:name (point-name bikepoint)
+   :bikes-free (bikes-available bikepoint)
+   :lat-lon (point-lat-lon bikepoint)})
+
+(defn simple-list [bikepoints]
+  (map point->simplepoint bikepoints))
+
+;(def adprop (:additionalProperties feed))
+;(remove nil? (map #(if (= "NbBikes" (:key %)) (:value %)) adprop))
